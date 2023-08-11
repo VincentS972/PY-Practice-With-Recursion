9451e4ccbe5c0d939e95574499075b1820ccf41d
VincentS972 <vincentsechi@gmail.com>
2023-08-10 20:32:43 -0400

    Initial commit

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 1 below
+
+def one_function(num):
+    if num == 0:
+        return 0
+
+    else:
+        print(num)
+        return (num * one_function(num-1))
+
+
+n=5
+one_function(n)
